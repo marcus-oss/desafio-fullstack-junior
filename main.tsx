5f70cb9bbf661c3cb4214a57337e9bddcbdd0046
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2026-05-15 10:38:18 -0300

    Add React Query Client Provider to main.tsx

---
+++ main.tsx
@@ -0,0 +1,7 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+<QueryClientProvider client={queryClient}>
+  <App />
+</QueryClientProvider>
