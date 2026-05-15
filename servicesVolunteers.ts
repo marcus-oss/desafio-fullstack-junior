63f8c6c32248e3d6e2f2b9a8f2ce7b484d60eb59
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2026-05-15 10:56:59 -0300

    Add CRUD functions for volunteer management

--- servicesVolunteers.ts
+++ servicesVolunteers.ts
@@ -1,6 +1,28 @@
 import { api } from "../api/axios";
+import { Volunteer } from "../types/volunteer";
 
 export async function getVolunteers() {
   const response = await api.get("/volunteers");
+  return response.data;
+}
+
+export async function createVolunteer(data: Partial<Volunteer>) {
+  const response = await api.post("/volunteers", data);
+  return response.data;
+}
+
+export async function updateVolunteer(
+  id: number,
+  data: Partial<Volunteer>
+) {
+  const response = await api.put(`/volunteers/${id}`, data);
+  return response.data;
+}
+
+export async function inactiveVolunteer(id: number) {
+  const response = await api.patch(`/volunteers/${id}`, {
+    status: "inativo",
+  });
+
   return response.data;
 }
