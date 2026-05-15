bf3522325d18038ae96bf1d7402c81fb0cc29291
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2026-05-15 10:47:48 -0300

    Refactor main.tsx to use ReactDOM.createRoot

--- main.tsx
+++ main.tsx
@@ -1,7 +1,25 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-<QueryClientProvider client={queryClient}>
-  <App />
-</QueryClientProvider>
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <QueryClientProvider
+      client={queryClient}
+    >
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
